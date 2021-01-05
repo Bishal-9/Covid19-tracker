6821cf9d35b3e37732fd68cad97f4fe910e51f42
Bishal 9 <goonbishal.bg@gmail.com>
2021-01-05 20:36:57 +0530

    Getting Country list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core'
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [counteries, setCountries] = useState([])
+  const [country, setCountry] = useState('worldwide')
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+          name: country.country,
+          value: country.countryInfo.iso2
+        }))
+        setCountries(countries)
+      })
+    }
+    getCountriesData()
+  }, [])
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value
+    console.log(countryCode)
+    setCountry(countryCode)
+  }
+
   return (
     <div className="app">
-      <h1>Lets build an Covid19 Tracker App 😎😎</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">World Wide</MenuItem>
+            {
+              counteries.map((country) => {
+                return(
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                )
+              })
+            }
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
