faed61e3cbb4dccfc7374c3c7f2cc9e16d865e81
Bishal 9 <goonbishal.bg@gmail.com>
2021-01-06 13:18:19 +0530

    Structure of Covid Tracker, Create Header, Create Info Boxes, Create Table, Create Chart

--- src/App.js
+++ src/App.js
@@ -1,58 +1,93 @@
-import { FormControl, MenuItem, Select } from '@material-ui/core'
+import { FormControl, MenuItem, Select, Card, CardContent } from '@material-ui/core'
 import { useEffect, useState } from 'react';
 import './App.css';
 import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from "./Table";
+import LineGraph from "./LineGraph";
+import { sortData } from "./util";
 
 function App() {
 
   const [counteries, setCountries] = useState([])
   const [country, setCountry] = useState('worldwide')
+  const [countryInfo, setCountryInfo] = useState({})
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+    .then(response => response.json())
+    .then((data) => {
+      setCountryInfo(data)
+    })
+  }, [])
 
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch('https://disease.sh/v3/covid-19/countries')
-      .then((response) => response.json())
-      .then((data) => {
-        const countries = data.map((country) => ({
-          name: country.country,
-          value: country.countryInfo.iso2
-        }))
-        setCountries(countries)
-      })
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }))
+          const sortedData = sortData(data)
+          setTableData(sortedData)
+          setCountries(countries)
+        })
     }
     getCountriesData()
   }, [])
 
   const onCountryChange = async (event) => {
-    const countryCode = event.target.value
-    console.log(countryCode)
-    setCountry(countryCode)
+    const countryCode = event.target.value    
+
+    const url = countryCode ==='worldwide' ? 'https://disease.sh/v3/covid-19/all' : 'https://disease.sh/v3/covid-19/countries/' + countryCode + '?strict=true'
+
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode)      
+      setCountryInfo(data)
+    })
   }
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country} onChange={onCountryChange}>
-            <MenuItem value="worldwide">World Wide</MenuItem>
-            {
-              counteries.map((country) => {
-                return(
-                  <MenuItem value={country.value}>{country.name}</MenuItem>
-                )
-              })
-            }
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select variant="outlined" value={country} onChange={onCountryChange}>
+              <MenuItem value="worldwide">World Wide</MenuItem>
+              {
+                counteries.map((country) => {
+                  return (
+                    <MenuItem value={country.value}>{country.name}</MenuItem>
+                  )
+                })
+              }
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+        </div>
+
+        <Map />
       </div>
 
-      <div className="app__stats">
-        <InfoBox title="Coronavirus cases" cases={20} total={2000} />
-        <InfoBox title="Recovered" cases={20} total={2000} />
-        <InfoBox title="Deaths" cases={20} total={2000} />
-      </div>
-
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide new cases</h3>
+          <LineGraph />
+        </CardContent>
+      </Card>
     </div>
   );
 }
